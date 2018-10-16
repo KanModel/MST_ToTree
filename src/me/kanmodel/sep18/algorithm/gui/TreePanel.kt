3cb76f2e4207e0dd7d50bbf3fd60400b7ef9800b
Kan_Model <kgdwhsk@gmail.com>
2018-10-16 14:38:53 +0800

    点对点画图基于半径

--- src/me/kanmodel/sep18/algorithm/gui/TreePanel.kt
+++ src/me/kanmodel/sep18/algorithm/gui/TreePanel.kt
@@ -14,7 +14,7 @@ import javax.swing.JPanel
  * Time: 10:20
  */
 class TreePanel(val frame: JFrame?, private val cost: Array<IntArray>, private val coor: Array<IntArray> = DataHolder.defaultCoordinateGenerate(), val dim: Int = DataHolder.cost.size) : JPanel() {
-    val weightList = ArrayList<Weight>()
+    private val weightList = ArrayList<Weight>()
 
     init {
 //        background = Color.
@@ -47,6 +47,7 @@ class TreePanel(val frame: JFrame?, private val cost: Array<IntArray>, private v
     }
 
     private fun drawP2P(g: Graphics, x1: Int, y1: Int, x2: Int, y2: Int, w: Int = 0, firstName: String = "", secondName: String = "") {
+//        drawLine(g, x1, y1, x2, y2, w)
         drawLine(g, x1, y1, x2, y2, w)
         drawCircle(g, x1, y1, firstName)
         drawCircle(g, x2, y2, secondName)
@@ -56,26 +57,26 @@ class TreePanel(val frame: JFrame?, private val cost: Array<IntArray>, private v
         val g2d = g.create() as Graphics2D
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
         g2d.color = Color.pink
-        g2d.fillArc(x, y, RIDUS, RIDUS, 0, 360)
+        g2d.fillArc(x, y, RADIUS, RADIUS, 0, 360)
         g2d.color = Color.BLACK
-        g2d.drawString(name, (x + RIDUS / 2 - 5), (y + RIDUS / 2 + 5))//打印名字
+        g2d.drawString(name, (x + RADIUS / 2 - 5), (y + RADIUS / 2 + 5))//打印名字
         g2d.dispose()
     }
 
     private fun drawLine(g: Graphics, x1: Int, y1: Int, x2: Int, y2: Int, w: Int = 0) {
         val g2d = g.create() as Graphics2D
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
-        g2d.drawLine(x1 + 25, y1 + 25, x2 + 25, y2 + 25)
+        g2d.drawLine(x1 + RADIUS / 2, y1 + RADIUS / 2, x2 + RADIUS / 2, y2 + RADIUS / 2)
 //        g2d.color = Color.RED
         if (w != 0) {
-//            g2d.drawString(w.toString(), (x1 + x2 + RIDUS) / 2, (y1 + y2 + RIDUS) / 2)
-            weightList.add(Weight((x1 + x2 + RIDUS) / 2, (y1 + y2 + RIDUS) / 2, w))//记录权重稍后打印
+//            g2d.drawString(w.toString(), (x1 + x2 + RADIUS) / 2, (y1 + y2 + RADIUS) / 2)
+            weightList.add(Weight((x1 + x2 + RADIUS) / 2, (y1 + y2 + RADIUS) / 2, w))//记录权重稍后打印
         }
         g2d.dispose()
     }
 
     companion object {
-        const val RIDUS = 50
+        const val RADIUS = 30
         const val COEFFICIENT = 1
 
         fun getTreePanel(cost: Array<IntArray> = DataHolder.cost, coor: Array<IntArray> = DataHolder.defaultCoordinateGenerate()): TreePanel {
